30d549e7bab59e164209eeedd396b7dd031bc624
charityvanb <charityvanb@gmail.com>
2018-11-28 16:06:19 -0700

    added seeds and stuffs

--- migrations/20181128113331_students.js
+++ migrations/20181128113331_students.js
@@ -3,6 +3,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('students', (student) => {
     student.increments('id')
     student.string('name')
+    student.string('fave_animal')
     student.string('previous_occupation')
     student.decimal('hometown_lat')
     student.decimal('hometown_long')
