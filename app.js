f25c02a29740cc9a918381db542bae2ac72150a4
charityvanb <charityvanb@gmail.com>
2018-11-29 11:30:53 -0700

    made it so you can select by id

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const app = express()
 const port = process.env.PORT || 3001
 const knex = require('knex')
 const queries = require('./queries.js')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+app.use(bodyParser.json())
+app.use(cors())
 
 app.listen(port, () => {
     console.log('listening on ${port}')
@@ -12,3 +17,8 @@ app.get('/', (req, res) => {
     queries.listAll()
     .then(students => res.send(students))
 })
+
+app.get('/:id', (req, res) => {
+    queries.getById(req.params.id).then(students => res.send(students))
+})
+//the stuff on the video at 9:40ish is a problem
--- queries.js
+++ queries.js
@@ -3,5 +3,9 @@ const database = require('./database_connection')
 module.exports = {
     listAll(){
           return database('students')
+    },
+
+getById(id) {
+    return database('students').where('id', id)
     }
-  }  
+}
